3996faeb6b3fe46cd0cd07fcbc0a7171871706c3
Tatiana Kadykova <tanya-kta@bk.ru>
2023-05-01 23:08:04 +0300

    Authentication implementation

--- user_control/tests.py
+++ user_control/tests.py
@@ -1,3 +1,72 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from .views import get_random, get_access_token, get_refresh_token
 
-# Create your tests here.
+class TestGenericFunctions(APITestCase):
+    def test_get_random(self):
+        rand1 = get_random(10)
+        rand2 = get_random(10)
+        rand3 = get_random(15)
+
+        self.assertTrue(rand1)
+        self.assertNotEqual(rand1, rand2)
+        self.assertEqual(len(rand1), 10)
+        self.assertEqual(len(rand3), 15)
+
+    def test_get_access_token(self):
+        payload = {
+            "id": 1
+        }
+        token = get_access_token(payload)
+        self.assertTrue(token)
+
+    def test_refresh_token(self):
+        token = get_refresh_token()
+        self.assertTrue(token)
+
+class TestAuth(APITestCase):
+    login_url = "/user/login"
+    register_url = "/user/register"
+    refresh_url = "/user/refresh"
+
+    def test_register(self):
+        payload = {
+            "username": "tanya-kta",
+            "password": "tanya9911"
+        }
+
+        response = self.client.post(self.register_url, data=payload)
+        self.assertEqual(response.status_code, 201)
+
+    def test_login(self):
+        payload = {
+            "username": "tanya-kta",
+            "password": "tanya9911"
+        }
+
+        self.client.post(self.register_url, data=payload)
+        response = self.client.post(self.login_url, data=payload)
+        result = response.json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(result["access"])
+        self.assertTrue(result["refresh"])
+
+    def test_refresh(self):
+        payload = {
+            "username": "tanya-kta",
+            "password": "tanya9911"
+        }
+
+        self.client.post(self.register_url, data=payload)
+        response = self.client.post(self.login_url, data=payload)
+        refresh = response.json()['refresh']
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(refresh)
+
+        response = self.client.post(self.refresh_url, data={"refresh": refresh})
+        result = response.json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(result["access"])
+        self.assertTrue(result["refresh"])
