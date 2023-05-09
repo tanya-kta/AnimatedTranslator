8d3dca64430c28d649d95acc44f3d3bae193470f
Tatiana Kadykova <tanya-kta@bk.ru>
2023-05-09 05:03:00 +0300

    Translation update

--- message_control/views.py
+++ message_control/views.py
@@ -51,7 +51,7 @@ class MessageView(ModelViewSet):
         .prefetch_related("message_attachments")
     serializer_class = MessageSerializer
     permission_classes = (IsAuthenticatedCustom, )
-    
+
     def get_queryset(self):
         data = self.request.query_params.dict()
         user_id = data.get("user_id", None)
@@ -63,31 +63,29 @@ class MessageView(ModelViewSet):
                 Q(sender_id=active_user_id, receiver_id=user_id)).distinct()
         return self.queryset
 
-    """def list(self, request, *args, **kwargs):
+    def list(self, request, *args, **kwargs):
         from user_control.models import UserProfile, CustomUser
         data = self.request.query_params.dict()
         user_id = data.get("user_id", None)
         user = CustomUser.objects.filter(id=user_id).distinct()[0]
         language = UserProfile.objects.filter(user=user).distinct()[0].language
-        #print(language)
-        queryset = self.filter_queryset(self.get_queryset())
-        serializer = self.get_serializer(queryset, many=True)
-        response = serializer.data
-        for item in response:
-            if item["message"][0:4] != "http":
-                item["message"] = translate_text(item["message"], language)
-        #print(response)
-        return Response(response)"""
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
 
+        queryset = self.filter_queryset(self.get_queryset())
         page = self.paginate_queryset(queryset)
         if page is not None:
             serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
+            result = serializer.data
+            for item in result:
+                if item["message"][0:4] != "http":
+                    item["message"] = translate_text(item["message"], language)
+            return self.get_paginated_response(result)
 
         serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+        result = serializer.data
+        for item in result:
+            if item["message"][0:4] != "http":
+                item["message"] = translate_text(item["message"], language)
+        return Response(result)
 
     def create(self, request, *args, **kwargs):
         if hasattr(request.data, '_mutable'):
