d1f6b726f46fc024ec70cd474dfec2ede98673c2
ulyanovikovak <99291314+ulyanovikovak@users.noreply.github.com>
2024-06-03 16:37:51 +0300

    Update models.py is_active field

--- user_control/models.py
+++ user_control/models.py
@@ -36,7 +36,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     updated_at = models.DateTimeField(auto_now=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     is_online = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = "username"
